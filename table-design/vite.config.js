f09cdfdf88632b5ee172a692b16bd17da3d4f973
VASANTHAN07-coder <vasanthandg@gmail.com>
2026-06-10 13:12:41 +0530

    viteconfig

--- table-design/vite.config.js
+++ table-design/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Table-ui-reactjs/',
   server: {
     proxy: {
       '/api': {
